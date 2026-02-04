78c1a69ae022f2e8d5de20206ca0f3fa85d5dee3
davidveat1-max <davidveat1@gmail.com>
2026-02-04 17:14:27 -0600

    Create comandos.js

---
+++ comandos.js
@@ -0,0 +1,23 @@
+module.exports = {
+  '.menu': `📋 MENÚ DE COMANDOS
+.avisos
+.reglas
+.pagos
+.horarios
+.contacto
+.soporte
+.documentos
+.reunion
+.links
+.info`,
+  '.avisos': 'AVISOS IMPORTANTES...',
+  '.reglas': 'REGLAS DEL GRUPO...',
+  '.pagos': 'INFORMACIÓN DE PAGOS...',
+  '.horarios': 'HORARIOS...',
+  '.contacto': 'CONTACTO...',
+  '.soporte': 'SOPORTE...',
+  '.documentos': 'DOCUMENTOS...',
+  '.reunion': 'PRÓXIMA REUNIÓN...',
+  '.links': 'ENLACES ÚTILES...',
+  '.info': 'INFORMACIÓN GENERAL...'
+}
